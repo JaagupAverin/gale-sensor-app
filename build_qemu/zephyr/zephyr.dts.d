4484c45a18b4cb0cf01c4eae1b44c968c26fa662
JaagupA <jaagup.averin@gmail.com>
2025-04-18 19:21:31 +0300

    Plz

---
+++ build_qemu/zephyr/zephyr.dts.d
@@ -0,0 +1,4 @@
+empty_file.o: /home/jaagup/projects/gale/zephyr/misc/empty_file.c \
+ /home/jaagup/projects/gale/zephyr/boards/qemu/riscv32/qemu_riscv32.dts \
+ /home/jaagup/projects/gale/zephyr/dts/riscv/qemu/virt-riscv32.dtsi \
+ /home/jaagup/projects/gale/zephyr/dts/riscv/qemu/virt-riscv.dtsi
