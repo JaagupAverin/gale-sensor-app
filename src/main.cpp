9f8200925a929173410b871b42681851160302e8
JaagupA <jaagup.averin@gmail.com>
2025-04-18 19:40:12 +0300

    Sensor app basics in QEMU

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 int main() {
-  printf("Hello, World!\n");
+  while (true) {
+    LOG_INF("Hello.");
+    k_sleep(K_MSEC(1000));
+  }
   return 0;
 }
